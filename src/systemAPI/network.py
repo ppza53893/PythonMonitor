57574721dba811201b2d109766985b7d605da4a6
ppza53893 <private.ukyo.info@gmail.com>
2021-12-10 16:55:53 +0900

    Update network.py

--- src/systemAPI/network.py
+++ src/systemAPI/network.py
@@ -1,11 +1,15 @@
 import time
-from typing import Tuple, Union
+from types import ModuleType
+from typing import Tuple, Union, Optional
 
 from ..utils import NetworkInterface, StatusContainer
 
 __all__ = ["Network"]
+NOT_CONNECTED = ['Not connected', 'Not connected']
 
 class Network():
+    MAX_WAIT_COUNT = 4
+
     def __init__(self):
         self._time = time.time()
         self._network = self._get_networks()
@@ -18,46 +22,59 @@ class Network():
         status = StatusContainer()
         adapters = NetworkInterface.GetAllNetworkInterfaces()
         for adapter in adapters:
-            stats = adapter.GetIPv4Statistics()
-            if adapter.Speed != -1 and stats.BytesReceived != 0:
+            sent, received = self._get_current_status(adapter)
+            if adapter.Speed != -1 and sent != 0 and received != 0:
                 status.register('adapter', adapter)
                 break
         return status
-    
-    def get_sent_received(self) -> Union[Tuple[float, float], Tuple[str, str]]:
-        if self._network.isempty:
-            if self._network_wait_count == 4:
-                self._network_wait_count = 0
-                self._network = self._get_networks()
-                return ['Connecting...']*2
-            self._network_wait_count += 1
-            return ['nan']*2
-        stats = self._network.adapter.GetIPv4Statistics()
+
+    def _get_current_status(self, adapter: Optional[ModuleType] = None) -> Tuple[float, float]:
+        if adapter is None:
+            stats = self._network.adapter.GetIPv4Statistics()
+        else:
+            stats = adapter.GetIPv4Statistics()
         current_sent = stats.BytesSent
         current_received = stats.BytesReceived
 
-        now = time.time()
-        time_diff = now - self._time
-        self._time = now
+        return current_sent, current_received
 
-        if self._sent is None or isinstance(self._sent, str):
-            self._sent = current_sent
-            self._received = current_received
-            return ['Connecting...']*2
-        elif current_sent < 0 or current_sent < self._sent:
-            self._sent = None
-            self._received = None
-            self._network_wait_count = 0
-            return ['nan']*2
-        elif current_sent == self._sent and current_received == self._received:
-            self._network_wait_count = 0
-            self._network = self._get_networks()
-            return ['nan']*2
-
+    def _reset_wait_count(self):
         self._network_wait_count = 0
-        sent = (current_sent - self._sent) / 1024 * time_diff
-        received = (current_received - self._received) / 1024 * time_diff
+    
+    def get_sent_received(self) -> Union[Tuple[float, float], Tuple[str, str]]:
+        if self._network.isempty:
+            if self._network_wait_count == self.MAX_WAIT_COUNT:
+                self._reset_wait_count()
+                self._network = self._get_networks()
+            else:
+                self._network_wait_count += 1
+            return NOT_CONNECTED
+        else:
+            current_sent, current_received = self._get_current_status()
+    
+            if self._sent is None or type(self._sent) is str:
+                self._sent = current_sent
+                self._received = current_received
+                return NOT_CONNECTED
+            elif current_sent < 0 or current_sent < self._sent:
+                self._sent = None
+                self._received = None
+                self._reset_wait_count()
+                return NOT_CONNECTED
+            elif current_sent == self._sent and current_received == self._received:
+                self._reset_wait_count()
+                self._network = self._get_networks()
+                return NOT_CONNECTED
+            else:
+                self._reset_wait_count()
+    
+                now = time.time()
+                time_diff = now - self._time
+                self._time = now
+    
+                sent = (current_sent - self._sent) / 1024 * time_diff
+                received = (current_received - self._received) / 1024 * time_diff
 
-        self._sent = current_sent
-        self._received = current_received
-        return [sent, received]
+                self._sent = current_sent
+                self._received = current_received
+                return [sent, received]
