5fd04a0104550cbb803ac9ab0122ff9929ee26e2
ppza53893 <private.ukyo.info@gmail.com>
2021-12-13 11:16:05 +0900

    pythonwではnvidia-smiを呼ばないように変更

--- src/systemAPI/gpu.py
+++ src/systemAPI/gpu.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import subprocess
 from typing import Union
 
@@ -9,26 +11,31 @@ NVSMI_QUERIES = [
     '--format=csv,noheader,nounits']
 GPU_MAX_POWER = None
 GPU_FAN_SPEED = None
+EXECUTABLE_NAME = os.path.splitext(os.path.basename(sys.executable))[0]
 
 
 def nvidia_smi_update() -> None:
     global GPU_MAX_POWER, GPU_FAN_SPEED
-    if GPU_MAX_POWER != 'NOT_AVAILABLE' and GPU_FAN_SPEED != 'NOT_AVAILABLE':
-        try:
-            enf_power, fan_speed = map(float,
-                        subprocess.run(NVSMI_QUERIES, stdout=subprocess.PIPE)\
-                        .stdout.decode('utf-8').replace('\r', '').replace('\n', '').replace(' ', '').split(','))
-            GPU_MAX_POWER = enf_power
-            GPU_FAN_SPEED = fan_speed
-        except:
-            GPU_MAX_POWER = 'NOT_AVAILABLE'
-            GPU_FAN_SPEED = 'NOT_AVAILABLE'
+    if GPU_MAX_POWER != 'DISABLERD' and GPU_FAN_SPEED != 'DISABLERD':
+        if EXECUTABLE_NAME != 'pythonw':
+            try:
+                enf_power, fan_speed = map(float,
+                            subprocess.run(NVSMI_QUERIES, stdout=subprocess.PIPE)\
+                            .stdout.decode('utf-8').replace('\r', '').replace('\n', '').replace(' ', '').split(','))
+                GPU_MAX_POWER = enf_power
+                GPU_FAN_SPEED = fan_speed
+            except:
+                GPU_MAX_POWER = 'DISABLERD'
+                GPU_FAN_SPEED = 'DISABLERD'
+        else:
+            GPU_MAX_POWER = 'DISABLERD'
+            GPU_FAN_SPEED = 'DISABLERD'
 
 
 def is_nvidia_smi_available() -> bool:
     if GPU_MAX_POWER is None or GPU_FAN_SPEED is None:
         nvidia_smi_update()
-    return GPU_MAX_POWER != 'NOT_AVAILABLE' and GPU_FAN_SPEED != 'NOT_AVAILABLE'
+    return GPU_MAX_POWER != 'DISABLERD' and GPU_FAN_SPEED != 'DISABLERD'
 
 
 def gpu_power_limit() -> Union[float, str]:
