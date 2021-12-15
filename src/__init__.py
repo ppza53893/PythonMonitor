a19026b624d2cd110eaa042de0751d798a45d764
ppza53893 <private.ukyo.info@gmail.com>
2021-12-15 10:28:58 +0900

    ワイルドカードインポート廃止
    他の場所については今後順次変えていく予定

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,92 @@
-from .gname import *
-from .ohmAPI import *
-from .systemAPI import *
-from .utils import *
-from .mpl_graph import *
+# gname.py
+from .gname import PYTASKMGR
+from .gname import AC_STATUS
+from .gname import BATTERY
+from .gname import BATTERY_STATUS
+from .gname import GPU_FAN
+from .gname import GPU_POWER
+from .gname import GPU_RAM
+from .gname import GPU_TEMP
+from .gname import CPU_TEMP
+from .gname import CPU_LOAD
+from .gname import CPU_CLOCK
+from .gname import CPU_POWER
+from .gname import DISK_USAGE
+from .gname import MEMORY_USAGE
+from .gname import RUN_PID
+from .gname import NET_SENT
+from .gname import NET_RECV
+
+# ohmAPI.py
+from .ohmAPI import OpenHardwareMonitor
+
+# systemAPI
+## network.py
+from .systemAPI import Network
+## powerline.py
+from .systemAPI import alert_on_balloontip
+from .systemAPI import get_battery_status
+## process.py
+# from .systemAPI import diskpartition
+# from .systemAPI import bios
+# from .systemAPI import battery
+# from .systemAPI import memory
+# from .systemAPI import operating_system
+# from .systemAPI import num_processors
+# from .systemAPI import cpu_usage
+from .systemAPI import get_current_pids
+from .systemAPI import c_disk_usage
+## win_forms.py
+from .systemAPI import error
+from .systemAPI import info
+from .systemAPI import question
+from .systemAPI import ans_yes
+from .systemAPI import show_message_to_notification
+from .systemAPI import workingarea
+from .systemAPI import borders
+from .systemAPI import set_icon
+## gpu.py
+from .systemAPI import nvidia_smi_update
+from .systemAPI import is_nvidia_smi_available
+from .systemAPI import gpu_power_limit
+from .systemAPI import gpu_fan_speed
+## systemAPi/__init__.py
+from .systemAPI import check_admin
+
+# util
+## container.py
+# from .utils import StatusContainer
+## pythonnet.py
+# from .utils import import_module
+## csharp_modules.py
+# from .utils import dispose
+# from .utils import System
+# from .utils import system
+# from .utils import Diagnostics
+# from .utils import diagnostics
+# from .utils import Management
+# from .utils import management
+# from .utils import Forms
+# from .utils import forms
+# from .utils import container
+# from .utils import Container
+# from .utils import Icon
+# from .utils import SystemIcons
+# from .utils import NetworkInterface
+# from .utils import close_container
+## table.py
+from .utils import Name
+from .utils import TableGroup
+from .utils import adjust_format
+from .utils import default_color
+from .utils import set_temperature_color
+from .utils import set_battery_color
+from .utils import set_load_color
+from .utils import set_system_color
+from .utils import set_nvgpu_power_color
+from .utils import set_nvgpu_fan_color
+
+# mplgraph
+from .mpl_graph import create_graph
+from .mpl_graph import has_mpl
+from .mpl_graph import use_dark_style
