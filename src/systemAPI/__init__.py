892e7d762b71d77ddf40a1d9bb9e5a1389ed9c40
ppza53893 <private.ukyo.info@gmail.com>
2021-12-16 13:45:23 +0900

    ワイルドカード廃止

--- src/systemAPI/__init__.py
+++ src/systemAPI/__init__.py
@@ -1,9 +1,31 @@
-from .network import *
-from .powerline import *
-from .process import *
-from .win_forms import *
-from .gpu import *
+from .network import Network
 
+from .powerline import alert_on_balloontip
+from .powerline import get_battery_status
+
+from .process import diskpartition
+from .process import bios
+from .process import battery
+from .process import memory
+from .process import operating_system
+from .process import num_processors
+from .process import cpu_usage
+from .process import get_current_pids
+from .process import c_disk_usage
+
+from .win_forms import error
+from .win_forms import info
+from .win_forms import question
+from .win_forms import ans_yes
+from .win_forms import show_message_to_notification
+from .win_forms import workingarea
+from .win_forms import borders
+from .win_forms import set_icon
+
+from .gpu import nvidia_smi_update
+from .gpu import is_nvidia_smi_available
+from .gpu import gpu_power_limit
+from .gpu import gpu_fan_speed
 
 def check_admin():
     import ctypes
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,30 @@
-from .container import *
-from .pythonnet import *
-from .csharp_modules import *
-from .table import *
+from .container import StatusContainer
+
+from .pythonnet import import_module
+
+from .csharp_modules import dispose
+from .csharp_modules import System
+from .csharp_modules import system
+from .csharp_modules import Diagnostics
+from .csharp_modules import diagnostics
+from .csharp_modules import Management
+from .csharp_modules import management
+from .csharp_modules import Forms
+from .csharp_modules import forms
+from .csharp_modules import container
+from .csharp_modules import Container
+from .csharp_modules import Icon
+from .csharp_modules import SystemIcons
+from .csharp_modules import NetworkInterface
+from .csharp_modules import close_container
+
+from .table import Name
+from .table import TableGroup
+from .table import adjust_format
+from .table import default_color
+from .table import set_temperature_color
+from .table import set_battery_color
+from .table import set_load_color
+from .table import set_system_color
+from .table import set_nvgpu_power_color
+from .table import set_nvgpu_fan_color
